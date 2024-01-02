558ae64fda24b2b73b4b1d563b59a5541a17da85
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-01-02 04:51:24 +0000

    Lots of back and forth and making changes, but adjusted everything to be end result of View Templates lesson

--- dice.rb
+++ dice.rb
@@ -20,21 +20,25 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
 
   erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
 
   erb(:two_ten)
 end
@@ -46,16 +50,27 @@ get("/dice/1/20") do
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
+  @rolls = []
 
-  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
 
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
 
 # for first part has currently taken 15 min for the lesson on erb stuff
